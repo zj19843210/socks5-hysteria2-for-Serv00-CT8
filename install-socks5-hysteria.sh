df0e4496170747f9e45db418dedd76b09b73b3b6
gshtwy <161450968+gshtwy@users.noreply.github.com>
2024-08-02 21:53:06 +0800

    Update install-socks5-hysteria.sh

--- install-socks5-hysteria.sh
+++ install-socks5-hysteria.sh
@@ -14,6 +14,8 @@ echo -e "\e[32m
 USER=$(whoami)
 WORKDIR="/home/${USER,,}/.nezha-agent"
 FILE_PATH="/home/${USER,,}/.s5"
+mkdir -p "$WORKDIR"
+mkdir -p "$FILE_PATH"
 
 ###################################################
 
@@ -35,9 +37,9 @@ download_dependencies() {
   mkdir -p "$DOWNLOAD_DIR"
   FILE_INFO=()
 
-  if [ "$ARCH" == "arm" ] || [ "$ARCH" == "arm64" ] || [ "$ARCH" == "aarch64" ]; then
+  if [[ "$ARCH" == "arm"* || "$ARCH" == "aarch64" ]]; then
     FILE_INFO=("https://download.hysteria.network/app/latest/hysteria-freebsd-arm64 web" "https://github.com/eooce/test/releases/download/ARM/swith npm")
-  elif [ "$ARCH" == "amd64" ] || [ "$ARCH" == "x86_64" ] || [ "$ARCH" == "x86" ]; then
+  elif [[ "$ARCH" == "amd64" || "$ARCH" == "x86_64" || "$ARCH" == "x86" ]]; then
     FILE_INFO=("https://download.hysteria.network/app/latest/hysteria-freebsd-amd64 web" "https://github.com/eooce/test/releases/download/freebsd/swith npm")
   else
     echo "不支持的架构: $ARCH"
@@ -52,11 +54,14 @@ download_dependencies() {
       echo -e "\e[1;32m$FILENAME 已存在，跳过下载\e[0m"
     else
       curl -L -sS -o "$FILENAME" "$URL"
+      if [ $? -ne 0 ]; then
+        echo -e "\e[1;31m下载 $FILENAME 失败\e[0m"
+        exit 1
+      fi
       echo -e "\e[1;32m下载 $FILENAME\e[0m"
     fi
-    chmod +x $FILENAME
+    chmod +x "$FILENAME"
   done
-  wait
 }
 
 # 生成证书函数
@@ -96,7 +101,15 @@ run_files() {
   if [ -e "$WORKDIR/web" ]; then
     nohup "$WORKDIR/web" server "$WORKDIR/config.yaml" >/dev/null 2>&1 &
     sleep 1
-    echo -e "\e[1;32mweb 正在运行\e[0m"
+    if pgrep -f "$WORKDIR/web" > /dev/null; then
+      echo -e "\e[1;32mweb 正在运行\e[0m"
+    else
+      echo -e "\e[1;31mweb 启动失败\e[0m"
+      exit 1
+    fi
+  else
+    echo -e "\e[1;31m$WORKDIR/web 不存在\e[0m"
+    exit 1
   fi
 }
 
@@ -112,7 +125,7 @@ get_ip() {
     else
       echo -e "\e[1;35m无法获取IPv4或IPv6地址\033[0m"
       exit 1
-    fi
+    }
   fi
   echo -e "\e[1;32m本机IP: $HOST_IP\033[0m"
 }
@@ -153,24 +166,24 @@ cleanup() {
 }
 
 # 安装和配置 socks5
-socks5_config(){
-# 提示用户输入 socks5 端口号
-read -p "请输入 socks5 端口号: " SOCKS5_PORT
+socks5_config() {
+  # 提示用户输入 socks5 端口号
+  read -p "请输入 socks5 端口号: " SOCKS5_PORT
 
-# 提示用户输入用户名和密码
-read -p "请输入 socks5 用户名: " SOCKS5_USER
+  # 提示用户输入用户名和密码
+  read -p "请输入 socks5 用户名: " SOCKS5_USER
 
-while true; do
-  read -p "请输入 socks5 密码（不能包含@和:）：" SOCKS5_PASS
-  echo
-  if [[ "$SOCKS5_PASS" == *"@"* || "$SOCKS5_PASS" == *":"* ]]; then
-    echo "密码中不能包含@和:符号，请重新输入。"
-  else
-    break
-  fi
-done
+  while true; do
+    read -p "请输入 socks5 密码（不能包含@和:）：" SOCKS5_PASS
+    echo
+    if [[ "$SOCKS5_PASS" == *"@"* || "$SOCKS5_PASS" == *":"* ]]; then
+      echo "密码中不能包含@和:符号，请重新输入。"
+    else
+      break
+    fi
+  done
 
-# config.js 文件
+  # config.json 文件
   cat > ${FILE_PATH}/config.json << EOF
 {
   "log": {
@@ -207,7 +220,7 @@ done
 EOF
 }
 
-install_socks5(){
+install_socks5() {
   socks5_config
   if [ ! -e "${FILE_PATH}/s5" ]; then
     curl -L -sS -o "${FILE_PATH}/s5" "https://github.com/eooce/test/releases/download/freebsd/web"
@@ -225,166 +238,22 @@ install_socks5(){
     chmod 777 "${FILE_PATH}/s5"
     nohup ${FILE_PATH}/s5 -c ${FILE_PATH}/config.json >/dev/null 2>&1 &
     sleep 2
-    pgrep -x "s5" > /dev/null && echo -e "\e[1;32ms5 正在运行\e[0m" || { echo -e "\e[1;35ms5 未运行，重启中...\e[0m"; pkill -x "s5" && nohup "${FILE_PATH}/s5" -c ${FILE_PATH}/config.json >/dev/null 2>&1 & sleep 2; echo -e "\e[1;32ms5 重启成功\e[0m"; }
-    CURL_OUTPUT=$(curl -s ip.sb --socks5 $SOCKS5_USER:$SOCKS5_PASS@localhost:$SOCKS5_PORT)
-    if [[ $CURL_OUTPUT =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-      echo "代理创建成功，返回的IP是: $CURL_OUTPUT"
-      echo "socks://${SOCKS5_USER}:${SOCKS5_PASS}@${CURL_OUTPUT}:${SOCKS5_PORT}"
-    else
-      echo "代理创建失败，请检查自己输入的内容。"
-    fi
-  fi
-}
-
-# 安装和配置 Nezha Agent
-download_agent() {
-    DOWNLOAD_LINK="https://github.com/nezhahq/agent/releases/latest/download/nezha-agent_freebsd_amd64.zip"
-    if ! wget -qO "$ZIP_FILE" "$DOWNLOAD_LINK"; then
-        echo 'error: Download failed! Please check your network or try again.'
-        return 1
-    fi
-    return 0
-}
-
-decompression() {
-    unzip "$1" -d "$TMP_DIRECTORY"
-    EXIT_CODE=$?
-    if [ ${EXIT_CODE} -ne 0 ]; then
-        rm -r "$TMP_DIRECTORY"
-        echo "removed: $TMP_DIRECTORY"
-        exit 1
-    fi
-}
-
-install_agent() {
-    install -m 755 ${TMP_DIRECTORY}/nezha-agent ${WORKDIR}/nezha-agent
-}
-
-generate_run_agent(){
-    echo "关于接下来需要输入的三个变量，请注意："
-    echo "Dashboard 站点地址可以写 IP 也可以写域名（域名不可套 CDN）;但是请不要加上 http:// 或者 https:// 等前缀，直接写 IP 或者域名即可；"
-    echo "面板 RPC 端口为你的 Dashboard 安装时设置的用于 Agent 接入的 RPC 端口（默认 5555）；"
-    echo "Agent 密钥需要先在管理面板上添加 Agent 获取。"
-    printf "请输入 Dashboard 站点地址："
-    read -r NZ_DASHBOARD_SERVER
-    printf "请输入面板 RPC 端口："
-    read -r NZ_DASHBOARD_PORT
-    printf "请输入 Agent 密钥: "
-    read -r NZ_DASHBOARD_PASSWORD
-    printf "是否启用针对 gRPC 端口的 SSL/TLS加密 (--tls)，需要请按 [Y]，默认是不需要，不理解的用户可回车跳过: "
-    read -r NZ_GRPC_PROXY
-    echo "${NZ_GRPC_PROXY}" | grep -qiw 'Y' && ARGS='--tls'
-
-    if [ -z "${NZ_DASHBOARD_SERVER}" ] || [ -z "${NZ_DASHBOARD_PASSWORD}" ]; then
-        echo "error! 所有选项都不能为空"
-        return 1
-        rm -rf ${WORKDIR}
-        exit
-    fi
-
-    cat > ${WORKDIR}/start.sh << EOF
-#!/bin/bash
-pgrep -f 'nezha-agent' | xargs -r kill
-cd ${WORKDIR}
-TMPDIR="${WORKDIR}" exec ${WORKDIR}/nezha-agent -s ${NZ_DASHBOARD_SERVER}:${NZ_DASHBOARD_PORT} -p ${NZ_DASHBOARD_PASSWORD} --report-delay 4 --disable-auto-update --disable-force-update ${ARGS} >/dev/null 2>&1
-EOF
-    chmod +x ${WORKDIR}/start.sh
-}
-
-run_agent(){
-    nohup ${WORKDIR}/start.sh >/dev/null 2>&1 &
-    printf "nezha-agent已经准备就绪，请按下回车键启动\n"
-    read
-    printf "正在启动nezha-agent，请耐心等待...\n"
-    sleep 3
-    if pgrep -f "nezha-agent -s" > /dev/null; then
-        echo "nezha-agent 已启动！"
-        echo "如果面板处未上线，请检查参数是否填写正确，并停止 agent 进程，删除已安装的 agent 后重新安装！"
-        echo "停止 agent 进程的命令：pgrep -f 'nezha-agent' | xargs -r kill"
-        echo "删除已安装的 agent 的命令：rm -rf ~/.nezha-agent"
-    else
-        rm -rf "${WORKDIR}"
-        echo "nezha-agent 启动失败，请检查参数填写是否正确，并重新安装！"
-    fi
-}
-
-install_nezha_agent(){
-  mkdir -p ${WORKDIR}
-  cd ${WORKDIR}
-  TMP_DIRECTORY="$(mktemp -d)"
-  ZIP_FILE="${TMP_DIRECTORY}/nezha-agent_freebsd_amd64.zip"
-
-  [ ! -e ${WORKDIR}/start.sh ] && generate_run_agent
-  [ ! -e ${WORKDIR}/nezha-agent ] && download_agent \
-  && decompression "${ZIP_FILE}" \
-  && install_agent
-  rm -rf "${TMP_DIRECTORY}"
-  [ -e ${WORKDIR}/start.sh ] && run_agent
-}
-
-install_hysteria(){
-  set_server_port
-  generate_password
-  download_dependencies
-  generate_cert
-  generate_config
-  run_files
-  get_ip
-  get_ipinfo
-  print_config
-  cleanup
-}
-
-########################梦开始的地方###########################
-
-read -p "是否安装 socks5 (Y/N 回车N): " socks5choice
-socks5choice=${socks5choice^^} # 转换为大写
-if [ "$socks5choice" == "Y" ]; then
-  # 检查socks5目录是否存在
-  if [ -d "$FILE_PATH" ]; then
-    install_socks5
+    pgrep -x "s5" > /dev/null && echo -e "\e[1;32ms5 正在运行\e[0m" || { echo -e "\e[1;35ms5 未运行，重试安装\e[0m"; exit 1; }
   else
-    # 创建socks5目录
-    echo "正在创建 socks5 目录..."
-    mkdir -p "$FILE_PATH"
-    install_socks5
+    echo -e "\e[1;35ms5 程序不存在\e[0m"
+    exit 1
   fi
-else
-  echo "不安装 socks5"
-fi
+}
 
-read -p "是否安装 hysteria (Y/N 回车N): " hysteriachoice
-hysteriachoice=${hysteriachoice^^} # 转换为大写
-if [ "$hysteriachoice" == "Y" ]; then
-  # 检查 hysteria 目录是否存在
-  if [ -d "$WORKDIR" ]; then
-    install_hysteria
-  else
-    # 创建 hysteria 目录
-    echo "正在创建 hysteria 目录..."
-    mkdir -p "$WORKDIR"
-    install_hysteria
-  fi
-else
-  echo "不安装 hysteria"
-fi
-
-read -p "是否安装 nezha-agent (Y/N 回车N): " choice
-choice=${choice^^} # 转换为大写
-if [ "$choice" == "Y" ]; then
-  echo "正在安装 nezha-agent..."
-  install_nezha_agent
-else
-  echo "不安装 nezha-agent"
-fi
-
-read -p "是否添加 crontab 守护进程的计划任务(Y/N 回车N): " crontabgogogo
-crontabgogogo=${crontabgogogo^^} # 转换为大写
-if [ "$crontabgogogo" == "Y" ]; then
-  echo "添加 crontab 守护进程的计划任务"
-  curl -s https://raw.githubusercontent.com/cmliu/socks5-for-serv00/main/check_cron.sh | bash
-else
-  echo "不添加 crontab 计划任务"
-fi
-
-echo "脚本执行完成。致谢：RealNeoMan、k0baya、eooce"
+# 主执行流程
+generate_password
+set_server_port
+download_dependencies
+generate_cert
+generate_config
+run_files
+get_ip
+get_ipinfo
+print_config
+cleanup
+install_socks5
